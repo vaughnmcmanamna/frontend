46c3d9a07a8cacafc0dc8e406b5ae4fcf7bb1034
Vaughn McManamna <vaughnmcmanamna03@gmail.com>
2025-11-27 17:32:09 -0800

    added starter code

---
+++ dropdown/script.js
@@ -0,0 +1,10 @@
+// Your data array
+const menuItems = ["Item 1", "Item 2", "Item 3"];
+
+// Select DOM elements
+const button = d3.select("#dropdown-btn");
+const menu = d3.select("#dropdown-menu");
+
+// Use D3 to bind data to list items
+// menu.selectAll("li")...
+// Add event listeners (button click to toggle menu, item clicks, etc.)
